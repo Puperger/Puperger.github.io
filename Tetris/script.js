6ec29607e04a821893c5bfc11fa5187b11bd543f
Puperger <82495364+Puperger@users.noreply.github.com>
2022-08-11 21:44:24 +0200

    Canvas Test

---
+++ Tetris/script.js
@@ -0,0 +1,39 @@
+
+
+var GameField = document.getElementById("GameBoard");
+var ctx = GameField.getContext("2d");
+
+
+
+var Board = [];
+
+
+
+
+var Pieces = [];
+
+function FillBoard(){
+    for (var i=0 ; i<20 ; i++){
+        Board.push([]);
+        for (var j=0 ; j<10 ; j++){
+            Board[i].push(0);
+        }
+    }
+}
+
+function DrawBoard(){
+    for (var i=0 ; i<20 ; i++){
+        for (var j=20 ; j<20 ; j++){
+
+        }
+    }
+}
+
+
+function StartGame(){
+    FillBoard()
+
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(0, 0, 150, 75);
+}
+
