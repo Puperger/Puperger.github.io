deca3290449ab5d5158c02faf472546948963b5a
Puperger <82495364+Puperger@users.noreply.github.com>
2022-08-13 08:49:22 +0200

    Redesign of index

--- scaleing.js
+++ scaleing.js
@@ -14,7 +14,7 @@ window.addEventListener('resize', function(event){
 
 updateSize = function(w,h){
     var boxes = document.getElementsByClassName("Box")
-    var boxW = parseInt((w-40)/2)
+    var boxW = parseInt((w-80)/2)
     for (let i = 0 ; i < boxes.length ; i++){
         boxes[i].style.height = boxW.toString()+"px";
         boxes[i].style.width = boxW.toString()+"px";
@@ -22,41 +22,14 @@ updateSize = function(w,h){
     }
     var thumbnails = document.getElementsByClassName("Thumbnail")
     for (let i = 0 ; i < thumbnails.length ; i++){
-        thumbnails[i].style.height = (boxW*2/3).toString()+"px";
-        thumbnails[i].style.width = boxW.toString()+"px";
+        thumbnails[i].style.height = ((boxW-10)*2/3).toString()+"px";
+        thumbnails[i].style.width = (boxW-10).toString()+"px";
     }
     var titles = document.getElementsByClassName("title")
     //Decide on Font Size based on screen size
-    if (w < 300){
         for (let i = 0 ; i < titles.length ; i++){
-            titles[i].style.fontSize = "10px";
-            titles[i].style.fontSize = "10px";
+            titles[i].style.marginTop = (boxW/12).toString()+"px";
         }
-    }
-    else if (w < 450){
-        for (let i = 0 ; i < titles.length ; i++){
-            titles[i].style.fontSize = "20px";
-            titles[i].style.fontSize = "20px";
-        }
-    }
-    else if(w < 900){
-        for (let i = 0 ; i < titles.length ; i++){
-            titles[i].style.fontSize = "30px";
-            titles[i].style.fontSize = "30px";
-        }
-    }
-    else if(w < 1800){
-        for (let i = 0 ; i < titles.length ; i++){
-            titles[i].style.fontSize = "50px";
-            titles[i].style.fontSize = "50px";
-        }
-    }
-    else if(w < 9000){
-        for (let i = 0 ; i < titles.length ; i++){
-            titles[i].style.fontSize = "75px";
-            titles[i].style.fontSize = "75px";
-        }
-    }
 }
 
 
