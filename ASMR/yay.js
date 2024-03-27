264aa6ed4abd42c775a595ca257f47ddc5079bab
japum0 <163680644+japum0@users.noreply.github.com>
2024-03-27 19:36:36 +0100

    ach
    ach

--- ASMR/yay.js
+++ ASMR/yay.js
@@ -5,3 +5,12 @@ function yay(t) {
         window.location.href = "https://puperger.ch/ASMR/yay.html";
     }
 }
+
+function ach(t) {
+    if (t == 0) {
+        let id = 'ach' + Math.ceil(Math.random()*6);
+        document.getElementById(id).play();
+    } else {
+        window.location.href = "https://puperger.ch/ASMR/ach.html";
+    }
+}
