854a8029d79609aeafa7e8185d6a532e9d5867a3
krfy43 <fkrebser@olympiad.ch>
2026-05-25 15:46:33 +0200

    ETH Website

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-function myFunction() {
-    var x = document.getElementById("header");
-    if (x.className === "head") {
-      x.className += " responsive";
-    } else {
-      x.className = "head";
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+//from: https://stackoverflow.com/a/39914235/14251221
+
+var str = "Physics" + ' ' + "Student @ ETH Zürich";
+var animate = document.getElementById('animate-title');
+async function typewriteTitle() {
+  var currentText = "";
+  for (let i = 0; i < str.length; i++) {
+    currentText += str.charAt(i);
+    if ((i%10) < 5)
+    animate.innerText = currentText + '_';
+    else 
+    animate.innerText = currentText + ' ';
+    await sleep(50 + Math.random() * 100);
+  }
+    for (let i = 0; i < 5; i++){
+        if (i % 2 == 0){
+            animate.innerText = currentText + ' ';
+        }
+        else{
+            animate.innerText = currentText + '_';
+        }
+        await sleep(500);
     }
-  } 
-
-
+}
+typewriteTitle();
--- update.bat
+++
@@ -1,3 +0,0 @@
-git add --all
-git commit -m "Bat file commit"
-git push
